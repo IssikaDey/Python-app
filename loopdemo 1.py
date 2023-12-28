f4070af0b20ab8ec66e87f3f84ed261d8cc90363
IssikaDey <155062702+IssikaDey@users.noreply.github.com>
2023-12-28 15:16:59 +0530

    Add files via upload

---
+++ loopdemo 1.py
@@ -0,0 +1,18 @@
+def displayNumbers1():
+    n=0
+    while n<=10: #Condition
+        print(n)
+        n=n+1
+#displayNumbers1()
+
+def displayNumbers2():
+    n=0
+    while True: #Infinite Loop
+        print(n)
+        n=n+1
+        if n>10:
+            break #Stop a loop
+displayNumbers2()       
+    
+
+    
